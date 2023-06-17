fb270501c57cb53d158d91008b5ed67c31c10f63
kento980037 <kento_0037@yahoo.co.jp>
2023-06-17 17:22:51 +0900

    add models of chatGPT

---
+++ model/request.go
@@ -0,0 +1,11 @@
+package model
+
+type RequestBody struct {
+	Model    string    `json:"model"`
+	Messages []Message `json:"messages"`
+}
+
+type Message struct {
+	Role    string `json:"role"`
+	Content string `json:"content"`
+}
---
+++ model/response.go
@@ -0,0 +1,22 @@
+package model
+
+type ResponseBody struct {
+	ID      string   `json:"id"`
+	Object  string   `json:"object"`
+	Created int      `json:"created"`
+	Model   string   `json:"model"`
+	Usage   Usage    `json:"usage"`
+	Choices []Choice `json:"choices"`
+}
+
+type Usage struct {
+	PromptTokens     int `json:"prompt_tokens"`
+	CompletionTokens int `json:"completion_tokens"`
+	TotalTokens      int `json:"total_tokens"`
+}
+
+type Choice struct {
+	Message      Message `json:"message"`
+	FinishReason string  `json:"finish_reason"`
+	Index        int     `json:"index"`
+}
